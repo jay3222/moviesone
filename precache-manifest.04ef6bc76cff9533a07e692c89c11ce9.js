3bcc754b732e1d6144727c37ec9de2b69af530bb
jayyadav <jayprakash9867@gmail.com>
2020-07-25 23:26:55 +0530

    Updates

---
+++ precache-manifest.04ef6bc76cff9533a07e692c89c11ce9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927322f1eb01b823b1c3b0b8b87cb218",
+    "url": "/moviesone/index.html"
+  },
+  {
+    "revision": "2b5a5b0f7404b6d54474",
+    "url": "/moviesone/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "fe2b5f94b1dcc9d292d4",
+    "url": "/moviesone/static/css/main.58109a07.chunk.css"
+  },
+  {
+    "revision": "2b5a5b0f7404b6d54474",
+    "url": "/moviesone/static/js/2.4558e701.chunk.js"
+  },
+  {
+    "revision": "4e5653c2aea1f8f01a4afdf4252ee778",
+    "url": "/moviesone/static/js/2.4558e701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe2b5f94b1dcc9d292d4",
+    "url": "/moviesone/static/js/main.3bbd37b2.chunk.js"
+  },
+  {
+    "revision": "ecf1b275d38acf07a9cc",
+    "url": "/moviesone/static/js/runtime-main.97922ef2.js"
+  },
+  {
+    "revision": "6ebd0bc15e9fb4df530297e0d4531390",
+    "url": "/moviesone/static/media/bahubali2.6ebd0bc1.jpg"
+  },
+  {
+    "revision": "02f69ce21ff6686ff567aecbc119ce60",
+    "url": "/moviesone/static/media/dark.02f69ce2.jpg"
+  },
+  {
+    "revision": "c93c7f15d54534a0473a6f77b8dfb3a4",
+    "url": "/moviesone/static/media/extra.c93c7f15.png"
+  },
+  {
+    "revision": "842800a6b923791c0592232cd0384e9a",
+    "url": "/moviesone/static/media/img1.842800a6.jpg"
+  },
+  {
+    "revision": "e032d23e2af396b5af02b960895435e3",
+    "url": "/moviesone/static/media/kesari.e032d23e.jpg"
+  },
+  {
+    "revision": "ed51a993ebcf65d8a199ac30d709d421",
+    "url": "/moviesone/static/media/stranger.ed51a993.jpg"
+  }
+]);
